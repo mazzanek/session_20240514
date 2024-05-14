3400141c38a7a1200b1a473ee3f0037f7afdc143
mazzanek <158159078+mazzanek@users.noreply.github.com>
2024-05-14 18:30:08 +0200

    minichyba

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Wrongmonth(Exception):
+    def __init__(self, message="Mesic mimo rozsah hahaha."):
+        self.message = message
+        super().__init__(self.message)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def zadejmesic():
+    try:
+        mesic = input("Zadejte mesic: ")
+        if mesic not in range(1, 13):
+            raise Wrongmonth
+    except TypeError:
+        print("Zadejte cislo!!!!")
+    except ValueError:
+       print("Zadejte cislo!!!!")
+    except Wrongmonth as bla:
+        #print("Mesic mimo rozsah.")
+        print(bla.message)
+    return mesic
+
+def obdobi(mesic):
+    if mesic in [1, 2, 12]:
+        obdobi = 1
+    elif mesic in [3, 4, 5]:
+        obdobi = 213
+    elif mesic in [6, 7, 8]:
+        obdobi = 3
+    elif mesic in [9, 10, 11]:
+        obdobi = 4
+    else:
+        obdobi = 5
+    return obdobi
+
+def main():
+    mesic = zadejmesic()
+    season = obdobi(mesic)
+    print(season)
+
+if __name__ == "__main__":
+    main()
